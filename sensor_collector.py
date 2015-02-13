4c64c9e56f0acd064b76570e5bb268241a2382b0
goliatone <burgosemi@gmail.com>
2015-02-12 22:17:22 -0500

    sensor_collector: Track movement

--- sensor_collector.py
+++ sensor_collector.py
@@ -1,18 +1,25 @@
-from grovepi import *
-from grove_rgb_lcd import *
+import grovepi
+# from grove_rgb_lcd import *
 import urllib, httplib
+import time
 
 ##This should be there now
 # Connect the DHt sensor to port 7
 dht_sensor_port = 7
 
+# Connect to PIR sensor, port 8
+pir_sensor = 8
+grovepi.pinMode(pir_sensor, "INPUT")
+
 while True:
     try:
         #Get the temperature and Humidity from the DHT sensor
-        [ temp, hum ] = dht(dht_sensor_port, 1)
+        [ temp, hum ] = grovepi.dht(dht_sensor_port, 1)
         print "temp =", temp, "C\thumadity =", hum,"%"
-
-        params = urllib.urlencode({'t': temp, 'h': hum, 'token':'YD4U41EMAHIGRLPE'})
+        #get movement
+        move = grovepi.digitalRead(pir_sensor)
+        print "movement=", move
+        params = urllib.urlencode({'t': temp, 'h': hum, 'm': move,  'token':'YD4U41EMAHIGRLPE'})
         headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
         conn = httplib.HTTPConnection("192.168.1.145:3000")
         try:
