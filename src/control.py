d0023a006ba74c90a7c4c52ff3bc37aeaef14f6e
goliatone <burgosemi@gmail.com>
2015-02-13 17:50:11 -0500

    control wip

--- src/control.py
+++ src/control.py
@@ -6,14 +6,14 @@ class Loop(object):
     """Loop"""
     def __init__(self, interval=1):
         super(Loop, self).__init__()
+        self._cmds =[]
         self.reset(interval=interval)
 
     def add_command(self, cmd):
         self._cmds.append(cmd)
 
     def reset(self, interval=1):
-        self._tick = 0
-        self._cmds =[]
+        self._tick = -1
         self._stopped = False
         self._interval = interval
 
@@ -27,17 +27,18 @@ class Loop(object):
     def _loop(self):
         while not self._stopped:
             self._tick += 1
-            for cmd in self._cmds:
+            for i, cmd in enumerate(self._cmds):
                 cmd.tick(self._tick)
             time.sleep(self._interval)
 
 
 class Command(object):
     """Command"""
-    def __init__(self, func, interval=5):
+    def __init__(self, func, interval=5, label="Command"):
         super(Command, self).__init__()
         self.func = func
         self._interval = interval
+        self.label = label
 
     def tick(self, count):
         if count % self._interval == 0:
