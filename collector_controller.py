9620fb369640a72cd46743960146d1e8df9c653a
goliatone <burgosemi@gmail.com>
2015-02-14 17:11:44 -0500

    collector controller wip

--- collector_controller.py
+++ collector_controller.py
@@ -8,11 +8,15 @@ import grovepi
 import urllib, httplib
 import time
 
+from src.control import Loop, Command
 
-class PIRReader(object):
+#TODO: Reader extend Command
+#TODO: Tie Command with URLRequest
+
+class PIRReader(Command):
     """PIRReader"""
-    def __init__(self, pin_number=8):
-        super(PIRReader, self).__init__()
+    def __init__(self, pin_number=8, interval=5, label="Command"):
+        super(Command, self, self.collect, interval, label).__init__()
         self._pin_number = pin_number
         self.value = None
         grovepi.pinMode(pin_number, "INPUT")
@@ -27,10 +31,10 @@ class PIRReader(object):
         return self.value
 
 
-class DHTReader(object):
+class DHTReader(Command):
     """DHTReader"""
     def __init__(self, pin_number=7):
-        super(DHTReader, self).__init__()
+        super(Command, self, self.collect, interval, label).__init__()
         self._pin_number = pin_number
         self.value = None
         grovepi.pinMode(pin_number, "INPUT")
@@ -69,46 +73,22 @@ class TapRequest(object):
             print "Error: ", e
 
 
-##This should be there now
-# Connect the DHt sensor to port 7
-dht_sensor_port = 7
-
-# Connect to PIR sensor, port 8
-pir_sensor = 8
-grovepi.pinMode(pir_sensor, "INPUT")
-
-while True:
-    try:
-        #Get the temperature and Humidity from the DHT sensor
-        [ temp, hum ] = grovepi.dht(dht_sensor_port, 1)
-        print "temp =", temp, "C\thumadity =", hum,"%"
-        #get movement
-        move = grovepi.digitalRead(pir_sensor)
-        print "movement=", move
-        params = urllib.urlencode({'t': temp, 'h': hum, 'm': move,  'token':'YD4U41EMAHIGRLPE'})
-        headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-        conn = httplib.HTTPConnection("192.168.1.145:3000")
-        try:
-            conn.request('POST', '/sensor/collect', params, headers)
-            # response = conn.getresponse()
-            # print response.status
-            # data = response.read()
-            conn.close()
-            time.sleep(16)
-        except Exception as e:
-            print "Error: ", e
-
-    except (IOError,TypeError) as e:
-        print "Error", e
-
-
-
 def main():
     parser = argparse.ArgumentParser(description='Sensor Collector controller')
     parser.add_argument('-H', '--host', required=True, help='Host to build HTTP connection URL')
     parser.add_argument('-P', '--port', required=True, help='Port to build HTTP connection URL')
     args = parser.parse_args()
 
+    motion = PIRReader(pin_number=8)
+    motionCommand = Command(motion.execute, interval=2, label="Motion Command")
+
+    climate = DHTReader(pin_number=7)
+    climateCommand = Command(climate.execute, interval=10, label="Climate Command")
+
+    loop = Loop()
+    loop.add_command(motionCommand)
+    loop.add_command(climateCommand)
+    loop.start()
 
 
 
