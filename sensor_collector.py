c4522969b5a34f086e6d20849f4d96e1c205b5b5
goliatone <burgosemi@gmail.com>
2015-02-14 17:12:02 -0500

    sensor_collector refactoring

--- sensor_collector.py
+++ sensor_collector.py
@@ -2,6 +2,10 @@ import grovepi
 # from grove_rgb_lcd import *
 import urllib, httplib
 import time
+import requests
+import json
+from collections import OrderedDict
+
 
 ##This should be there now
 # Connect the DHt sensor to port 7
@@ -9,28 +13,87 @@ dht_sensor_port = 7
 
 # Connect to PIR sensor, port 8
 pir_sensor = 8
-grovepi.pinMode(pir_sensor, "INPUT")
+grovepi.pinMode(pir_sensor, 'INPUT')
+
+URI = '/sensor/collect'
+HOST = '192.168.1.145'
+PORT = '3000'
+URL_TEMPLATE = 'http://%s:%s%s'
+
+
+def request(payload):
+    if not payload: return []
+
+    url = URL_TEMPLATE % (HOST, PORT, URI)
+    headers = {'content-type': 'application/json'}
+
+    try:
+        requests.post(url, data=json.dumps(payload), headers=headers)
+    except Exception as e:
+        print 'Error: ', e
+
+    return []
+
+
+def collect_dht(value=None):
+    if not value: value = {}
+    try:
+        [ temp, hum ] = grovepi.dht(dht_sensor_port, 1)
+        value.update({'t':temp, 'h': hum})
+    except (IOError, TypeError) as e:
+        print 'DHT IOError: ', e
+    return value
+
+
+def collect_pir(value=None):
+    if not value: value = {}
+    try:
+        move = grovepi.digitalRead(pir_sensor)
+        value.update({'m': move})
+    except (IOError, TypeError) as e:
+        print 'PIR IOError: ', e
+    return value
+
+
+def collect_timestamp(value):
+    value.update({'timestamp': int(time.time())})
+    return value
+
+
+def log(msg, params):
+    if DEBUG:
+        print msg.format(**params)
+
+
+count = 0
+value = {}
+params = []
+DEBUG = True
+
 
 while True:
     try:
-        #Get the temperature and Humidity from the DHT sensor
-        [ temp, hum ] = grovepi.dht(dht_sensor_port, 1)
-        print "temp =", temp, "C\thumadity =", hum,"%"
-        #get movement
-        move = grovepi.digitalRead(pir_sensor)
-        print "movement=", move
-        params = urllib.urlencode({'t': temp, 'h': hum, 'm': move,  'token':'YD4U41EMAHIGRLPE'})
-        headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-        conn = httplib.HTTPConnection("192.168.1.145:3000")
-        try:
-            conn.request('POST', '/sensor/collect', params, headers)
-            # response = conn.getresponse()
-            # print response.status
-            # data = response.read()
-            conn.close()
-            time.sleep(16)
-        except Exception as e:
-            print "Error: ", e
+
+        if count % 16 == 0:
+            value = collect_dht()
+            log('temp = {t} C\thumidity = {h}%', value)
+
+        if count % 2 == 0:
+            value = collect_pir(value)
+            log('movement= {m}', value)
+
+        if count % 2 == 0:
+            value = collect_timestamp(value)
+            # log('timestamp= {timestamp}', value)
+
+        if count % 32 == 0 and len(params) != 0:
+            params = [i for n, i in enumerate(params) if i not in params[n + 1:]]
+            print params
+            params = request(params)
+        else: params.append(value.copy())
+
+        count += 1
+        time.sleep(1)
 
     except (IOError,TypeError) as e:
-        print "Error", e
+        print 'Error: ', e
