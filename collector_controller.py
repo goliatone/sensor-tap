85401bc9f2578056a5aaed1730a5bf131a1fd412
goliatone <burgosemi@gmail.com>
2015-02-13 23:48:46 -0500

    WIP...

--- collector_controller.py
+++ collector_controller.py
@@ -18,8 +18,11 @@ class PIRReader(object):
         grovepi.pinMode(pin_number, "INPUT")
 
     def collect(self):
-        movement = grovepi.digitalRead(self._pin_number)
-        self.value = { "m": movement }
+        try:
+            movement = grovepi.digitalRead(self._pin_number)
+            self.value = { "m": movement }
+        except (IOError,TypeError) as e:
+            print "IOError", e
         if self.verbose: print "PIR: ", movement
         return self.value
 
@@ -33,12 +36,37 @@ class DHTReader(object):
         grovepi.pinMode(pin_number, "INPUT")
 
     def collect(self):
-        [ temp, hum ] = grovepi.dht(self._pin_number, 1)
-        self.value = { "t": temp, "h": hum }
+        try:
+            [ temp, hum ] = grovepi.dht(self._pin_number, 1)
+            self.value = { "t": temp, "h": hum }
+        except (IOError,TypeError) as e:
+            print "IOError", e
         if self.verbose: print "DHT: ", self.value
         return self.value
 
+class TapRequest(object):
+    """TapRequest"""
+    def __init__(self, host='localhost', port='8080'):
+        super(TapRequest, self).__init__()
+        self._host = host
+        self._port = port
+        self._endpoint = "%s:%s"%(host, port)
+        self.headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
 
+    def build_params(self):
+        self.params = self.build_params()
+
+    def build_request(self):
+        self._connection = httplib.HTTPConnection(self._endpoint)
+
+    def execute(self):
+        self.build_params()
+        self.build_request()
+        try:
+            self._connection.request('POST', self._endpoint, self.params, self.headers)
+            self._connection.close()
+        except Exception as e:
+            print "Error: ", e
 
 
 ##This should be there now
@@ -80,6 +108,7 @@ def main():
     parser.add_argument('-H', '--host', required=True, help='Host to build HTTP connection URL')
     parser.add_argument('-P', '--port', required=True, help='Port to build HTTP connection URL')
     args = parser.parse_args()
+
 
 
 
