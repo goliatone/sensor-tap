41a70adf1df2329e81a7afeeb6744515d6a89071
goliatone <burgosemi@gmail.com>
2015-02-14 20:19:40 -0500

    sensor_collector: Added light and noise sensors.

--- sensor_collector.py
+++ sensor_collector.py
@@ -1,24 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import grovepi
-# from grove_rgb_lcd import *
 import urllib, httplib
 import time
 import requests
 import json
 from collections import OrderedDict
-
-
-##This should be there now
-# Connect the DHt sensor to port 7
-dht_sensor_port = 7
-
-# Connect to PIR sensor, port 8
-pir_sensor = 8
-grovepi.pinMode(pir_sensor, 'INPUT')
-
-URI = '/sensor/collect'
-HOST = '192.168.1.145'
-PORT = '3000'
-URL_TEMPLATE = 'http://%s:%s%s'
 
 
 def request(payload):
@@ -38,8 +26,8 @@ def request(payload):
 def collect_dht(value=None):
     if not value: value = {}
     try:
-        [ temp, hum ] = grovepi.dht(dht_sensor_port, 1)
-        value.update({'t':temp, 'h': hum})
+        [ temp, hum ] = grovepi.dht(dht_sensor, 1)
+        value.update({'t': temp, 'h': hum})
     except (IOError, TypeError) as e:
         print 'DHT IOError: ', e
     return value
@@ -54,6 +42,24 @@ def collect_pir(value=None):
         print 'PIR IOError: ', e
     return value
 
+def collect_light(value=None):
+    if not value: value = {}
+    try:
+        light = grovepi.analogRead(light_sensor)
+        value.update({'l': light})
+    except (IOError, TypeError) as e:
+        print 'LIGHT SENSOR IOError: ', e
+    return value
+
+def collect_sound(value=None):
+    if not value: value = {}
+    try:
+        sound = grovepi.analogRead(sound_sensor)
+        value.update({'s': sound})
+    except (IOError, TypeError) as e:
+        print 'SOUND SENSOR IOError: ', e
+    return value
+
 
 def collect_timestamp(value):
     value.update({'timestamp': int(time.time())})
@@ -62,13 +68,33 @@ def collect_timestamp(value):
 
 def log(msg, params):
     if DEBUG:
-        print msg.format(**params)
+        try:
+            print msg.format(**params)
+        except Exception as e:
+            print "LOG Exception: %s" % e
 
+
+URI = '/sensor/collect'
+HOST = '192.168.1.145'
+PORT = '3000'
+URL_TEMPLATE = 'http://%s:%s%s'
 
 count = 0
 value = {}
 params = []
 DEBUG = True
+
+
+sound_sensor = 1 # Analog
+light_sensor = 2 # Analog
+dht_sensor = 7   # Digital
+pir_sensor = 8   # Digital
+
+grovepi.pinMode(pir_sensor, 'INPUT')
+
+
+def select_unique(data):
+    return [i for n, i in enumerate(data) if i not in data[n + 1:]]
 
 
 while True:
@@ -78,17 +104,21 @@ while True:
             value = collect_dht()
             log('temp = {t} C\thumidity = {h}%', value)
 
+        if count % 32 == 0:
+            value = collect_light()
+            log('light = {l}%', value)
+
         if count % 2 == 0:
+            value = collect_sound(value)
+            log('sound= {s}', value)
+        # if count % 2 == 0:
             value = collect_pir(value)
             log('movement= {m}', value)
-
-        if count % 2 == 0:
+        # if count % 2 == 0:
             value = collect_timestamp(value)
-            # log('timestamp= {timestamp}', value)
 
-        if count % 32 == 0 and len(params) != 0:
-            params = [i for n, i in enumerate(params) if i not in params[n + 1:]]
-            print params
+        if count % 48 == 0 and len(params) != 0:
+            params = select_unique(params)
             params = request(params)
         else: params.append(value.copy())
 
